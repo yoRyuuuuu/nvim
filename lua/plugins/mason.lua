8e9c3e8f81f8ddb0badad1b60e99ef00b313ba67
yoRyuuuuu <your-email@example.com>
2026-01-02 06:07:56 +0900

    Update maon config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,6 +1,25 @@
 return {
   {
-    "mason-org/mason.nvim",
-    opts = {},
+    {
+      "mason-org/mason.nvim",
+      opts = {},
+    },
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = { "mason-org/mason.nvim" },
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "gopls",
+        "golangci-lint",
+        "terraform",
+        "terraform-ls",
+        "tflint",
+        "lua-language-server",
+      },
+    },
   },
 }
