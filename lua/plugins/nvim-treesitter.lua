d0ee46933be3029e9c43016b5d84a60e50ed6523
yoRyuuuuu <your-email@example.com>
2026-01-02 04:02:33 +0900

    Add nvim-treesitter config

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+        "go",
+      },
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = true,
+      },
+    },
+  },
+}
