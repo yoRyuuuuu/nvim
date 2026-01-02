aee30c6427ec410591942cc002909998e6276110
yoRyuuuuu <your-email@example.com>
2026-01-02 17:11:41 +0900

    Update blink config

--- lua/plugins/editor/blink.lua
+++ lua/plugins/editor/blink.lua
@@ -5,7 +5,10 @@ return {
   opts = {
     keymap = { preset = "default" },
     appearance = { nerd_font_variant = "mono" },
-    completion = { documentation = { auto_show = false } },
+    completion = {
+      documentation = { auto_show = true },
+      list = { selection = { preselect = true, auto_insert = false } },
+    },
     sources = { default = { "lsp", "path", "snippets", "buffer" } },
     fuzzy = { implementation = "prefer_rust_with_warning" },
   },
