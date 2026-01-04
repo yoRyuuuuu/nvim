ec0d8d6ea981e3d63f7b1912bed893ed3f4e1d3f
yoRyuuuuu <your-email@example.com>
2026-01-05 03:41:06 +0900

    Update better-escape config

--- lua/plugins/editor/better-escape.lua
+++ lua/plugins/editor/better-escape.lua
@@ -2,8 +2,14 @@ return {
   "max397574/better-escape.nvim",
   event = "InsertEnter",
   opts = {
-    mapping = { "jk" },
-    timeout = vim.o.timeoutlen,
+    timeout = 100,
+    default_mappings = false,
+    mappings = {
+      i = { j = { k = "<ESC>" } },
+      t = {
+        j = { k = "<C-\\><C-n>" },
+      },
+    },
   },
   config = function(_, opts)
     require("better_escape").setup(opts)
