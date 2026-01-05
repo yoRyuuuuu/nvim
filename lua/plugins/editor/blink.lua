c7934a3a392f8deadd5a1e0ffdb1967503a5f688
yoRyuuuuu <ryugo.yoshioka@fuller.co.jp>
2026-01-05 13:09:04 +0900

    Update blink config

--- lua/plugins/editor/blink.lua
+++ lua/plugins/editor/blink.lua
@@ -11,6 +11,25 @@ return {
     },
     sources = { default = { "lsp", "path", "snippets", "buffer" } },
     fuzzy = { implementation = "prefer_rust_with_warning" },
+    cmdline = {
+      enabled = true,
+      completion = {
+        menu = { auto_show = true },
+        ghost_text = { enabled = true },
+      },
+      sources = function()
+        local type = vim.fn.getcmdtype()
+        -- 検索時はbufferから補完
+        if type == "/" or type == "?" then
+          return { "buffer" }
+        end
+        -- コマンドモードではcmdlineとpathから補完
+        if type == ":" then
+          return { "cmdline", "path" }
+        end
+        return {}
+      end,
+    },
   },
   opts_extend = { "sources.default" },
 }
