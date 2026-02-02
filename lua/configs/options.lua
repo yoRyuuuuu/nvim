59c8a7eb472cacbde8529e7c2bc8bac82fe448bf
yoRyuuuuu <ryugo.yoshioka@fuller.co.jp>
2026-02-02 21:36:53 +0900

    Update options config

--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -5,7 +5,7 @@ opt.autowrite = true -- Enable auto write
 -- integration works automatically.
 opt.clipboard = vim.env.SSH_CONNECTION and "" or "unnamedplus" -- Sync with system clipboard
 opt.completeopt = "menu,menuone,noselect"
-opt.conceallevel = 2 -- Hide * markup for bold and italic, but not markers with substitutions
+opt.conceallevel = 0 -- Show all markup characters (disable conceal)
 opt.confirm = true -- Confirm to save changes before exiting modified buffer
 opt.cursorline = true -- Enable highlighting of the current line
 opt.expandtab = true -- Use spaces instead of tabs
