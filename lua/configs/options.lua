853471c8a0129c3d4a13380782d21efe78fd6c7a
yoRyuuuuu <ryugo.yoshioka@fuller.co.jp>
2026-09-23 11:01:41 +0900

    Wrap lines at word boundaries in markdown buffers

--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -65,6 +65,15 @@ opt.cmdheight = 0
 -- Fix markdown indentation settings
 vim.g.markdown_recommended_style = 0
 
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("markdown_wrap", { clear = true }),
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
+  end,
+})
+
 -- Set <leader> key
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
